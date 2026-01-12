26e2db83c514dcb4decb5961db30af18f7108d47
Breno <brenojbarbosas@gmail.com>
2026-01-12 08:44:37 -0300

    atualizando

--- fast_car_api/app.py
+++ fast_car_api/app.py
@@ -1,19 +1,19 @@
 from fastapi import FastAPI
 from routes import router as car_router
 from database import Base, engine
-import models  # IMPORTANTE: força o registro dos models
+import models
 
 import os
 
 app = FastAPI(title="Fast Car Api")
 
-# 👇 Mostra exatamente ONDE o SQLite está criando o arquivo
+
 print("USANDO BANCO EM:", os.path.abspath("cars.db"))
 
-# 👇 Cria as tabelas se não existirem
+
 Base.metadata.create_all(bind=engine)
 
-# 👇 Registra as rotas
+
 app.include_router(car_router)
 
 @app.get("/")
